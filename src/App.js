121cf847a3c15003c2628fa8d4f2159475fedac1
bouchra <matrahbouchra@gmail.com>
2024-12-17 16:22:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import outilsImage from './outils.png';
+import SecondPage from './secondPage'; // Assurez-vous que le chemin est correct
 
 function App() {
+  const [showSecondPage, setShowSecondPage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSecondPage(true);
+    }, 3000); // Transition après 3 secondes
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSecondPage) {
+    return <SecondPage />;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img 
+          src={outilsImage} 
+          alt="Outils" 
+          style={{ width: '350px', height: 'auto' }} 
+        />
+        <h1 style={{ color: '#303030' }}><strong>Gérer Vos Contacts</strong></h1>
       </header>
     </div>
   );
 }
 
 export default App;
+
